79566bf2202baf8124198e89f38c0d47b5c3dec7
Suresh Koleti <skoleti@codeaurora.org>
2018-12-28 14:44:41 +0530

    sdm845: Include ims_ext_common permission xml.
    Change-Id: Ide3ecae57cc1909180c8d8f323aa31620e900888
    CRs-Fixed: 2373721

--- sdm845.mk
+++ sdm845.mk
@@ -161,6 +161,7 @@ PRODUCT_PACKAGES += \
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
+    ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
