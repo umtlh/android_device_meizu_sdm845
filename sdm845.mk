0503cf1ec187eac2ed52ff655ad4dbe7f9f1a22a
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 13:58:23 -0800

    sdm845: Use 6GB dalvik-heap config
    We are currently using 2GB dalvik-heap config.
    
    Bug: 145823510
    Test: MPTS test
    Change-Id: Ie26b8e4fefea25fa818bd1eaf9df4a0bb153d42d

--- sdm845.mk
+++ sdm845.mk
@@ -6,6 +6,9 @@
 
 $(call inherit-product, vendor/meizu/sdm845/sdm845-vendor.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-sdm845
