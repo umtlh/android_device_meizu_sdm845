e0ae3979d26a8b6b4ce8678476df3c9aeb944ed8
TRANSAERO21 <martynenkodima21@gmail.com>
2020-08-31 18:29:00 +0300

    m1882: init: flashlight effects again but ok))
    why not

--- init/vendor/bin/init.effects.sh
+++ init/vendor/bin/init.effects.sh
@@ -4,33 +4,123 @@ while [[ -z $(getprop sys.boot_completed) ]]
 do
   echo "7" > /sys/class/meizu/flash/flash4
 done
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
-sleep 0.1
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.025
 echo "4" > /sys/class/meizu/flash/flash4
