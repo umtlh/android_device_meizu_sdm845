e4b1d750f71e809415385de59b79c563d65511b4
TRANSAERO21 <martynenkodima21@gmail.com>
2020-08-31 18:22:40 +0300

    m1882: init: Have fun with flashlight effects)))

---
+++ init/vendor/bin/init.effects.sh
@@ -0,0 +1,36 @@
+#! /vendor/bin/sh
+
+while [[ -z $(getprop sys.boot_completed) ]]
+do
+  echo "7" > /sys/class/meizu/flash/flash4
+done
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
+sleep 0.1
+echo "4" > /sys/class/meizu/flash/flash4
