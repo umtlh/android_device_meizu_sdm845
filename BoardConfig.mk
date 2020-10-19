07c2dab6c3f178e33696e0f26d8db4a0745d659d
TRANSAERO21 <martynenkodima21@gmail.com>
2020-10-19 14:52:56 +0300

    m1882: Switch to another qcom sepolicy
    To Fix
    line 839 has invalid context u:object_r:firmware_file:s0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,7 +152,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2020-07-01
 
 # SELinux
-include device/qcom/sepolicy/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Soong namespaces
