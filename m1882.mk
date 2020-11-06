786da28d0eb1316e09b02f0dbe5a2c943ba8fe0f
TRANSAERO21 <martynenkodima21@gmail.com>
2020-11-06 10:54:33 +0300

    m1882: power: Import qti power 1.2 HAL

--- m1882.mk
+++ m1882.mk
@@ -207,7 +207,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.2-service-qti.m1882
 
 # QCOM
 PRODUCT_COPY_FILES += \
