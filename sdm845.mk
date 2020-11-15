09875c326816cbae2743a62cbdd3247f80e5f6b4
Chenyang Zhong <zhongcy95@gmail.com>
2020-11-15 02:43:45 -0500

    sdm845: Update CNE, IMS, QMI, and RIL blobs
    from LA.UM.9.8.r1-02700-SDM710.0
    
    Also, qti_libpermissions.xml is apparently not a patched version,
    which can be shown from both the file content:
    
    android.hidl.manager-V1.0-java             --- unpatched
    android.hidl.manager@1.0-java              --- supposedly patched
    
    ...and from the sha1sum. Therefore, remove the pinned hash.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -262,9 +262,9 @@ PRODUCT_COPY_FILES += \
 
 # Radio
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
-    android.hardware.radio.config@1.0 \
-    android.hardware.secure_element@1.0 \
+    android.hardware.radio@1.5 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.secure_element@1.2 \
     ims-ext-common \
     libjson \
     librmnetctl \
