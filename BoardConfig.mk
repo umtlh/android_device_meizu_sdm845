ce9c8e68bd03cf7932b8a3d362d489b6e7227241
TRANSAERO21 <martynenkodima21@gmail.com>
2020-12-05 20:56:10 +0300

    m1882: Kang 'libprotobuf-cpp-full-vendor-3.9.1.so' from LA.UM.9.8.r1-02700-SDM710.0
    needed to resolve symbols for 'android.hardware.drm@1.3-service.clearkey' and 'com.inv.node.eis.so'

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,7 @@ USE_CAMERA_STUB := true
 BOARD_QTI_CAMERA_32BIT_ONLY := true
 TARGET_LD_SHIM_LIBS += /vendor/lib/libmms_hal_vstab.so|/vendor/lib/libshim_camera.so
 TARGET_LD_SHIM_LIBS += /vendor/lib/libmms_warper_vstab.so|/vendor/lib/libshim_camera.so
+TARGET_LD_SHIM_LIBS += /vendor/lib/camera/components/com.inv.node.eis.so|/vendor/lib/libprotobuf-cpp-full-vendor-3.9.1.so
 
 # Charger
 WITH_LINEAGE_CHARGER := false
@@ -87,6 +88,7 @@ TARGET_SCREEN_DENSITY := 400
 
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
+TARGET_LD_SHIM_LIBS += /vendor/bin/hw/android.hardware.drm@1.3-service.clearkey|/vendor/lib64/libprotobuf-cpp-full-vendor-3.9.1.so
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
