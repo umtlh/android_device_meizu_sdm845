MoKee/android_device_meizu_sdm845-common

sdm845: parts: Trigger AOD each time screen is off
Change-Id: I9210bae2e837c857ef911925d3d50e40a2f63d47

parts/Android.mk
@@ -1,0 +1,25 @@
+#
+# Copyright (C) 2020 The MoKee Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := MeizuParts
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.mokee.platform.internal
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)

parts/src/org/mokee/device/parts/AODService.java
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.mokee.device.parts;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+
+import org.mokee.internal.util.FileUtils;
+
+public class AODService extends Service {
+
+    private static final String TAG = "AODService";
+    private static final boolean DEBUG = false;
+
+    private static final long AOD_DELAY_MS = 4000;
+
+    private SettingObserver mSettingObserver;
+    private ScreenReceiver mScreenReceiver;
+
+    private Handler mHandler = new Handler(Looper.getMainLooper());
+    private boolean mInteractive = true;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (DEBUG) Log.d(TAG, "Creating service");
+
+        mSettingObserver = new SettingObserver(this);
+        mScreenReceiver = new ScreenReceiver(this);
+
+        mSettingObserver.enable();
+
+        if (Utils.isAODEnabled(this)) {
+            mScreenReceiver.enable();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (DEBUG) Log.d(TAG, "Destroying service");
+
+        mSettingObserver.disable();
+        mScreenReceiver.disable();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    void onSettingChange() {
+        if (Utils.isAODEnabled(this)) {
+            Log.d(TAG, "AOD enabled");
+            mScreenReceiver.enable();
+        } else {
+            Log.d(TAG, "AOD disabled");
+            mScreenReceiver.disable();
+        }
+    }
+
+    void onDisplayOn() {
+        Log.d(TAG, "Device interactive");
+        mInteractive = true;
+        FileUtils.writeLine(Constants.AOD_ENABLE, "0");
+        mHandler.removeCallbacksAndMessages(null);
+    }
+
+    void onDisplayOff() {
+        Log.d(TAG, "Device non-interactive");
+        mInteractive = false;
+        mHandler.postDelayed(() -> {
+            if (!mInteractive) {
+                Log.d(TAG, "Trigger AOD");
+                FileUtils.writeLine(Constants.AOD_ENABLE, "1");
+            }
+        }, AOD_DELAY_MS);
+    }
+
+}

parts/src/org/mokee/device/parts/ScreenReceiver.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.mokee.device.parts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+
+class ScreenReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "AODScreen";
+    private static final boolean DEBUG = false;
+
+    private final AODService mService;
+
+    private final IntentFilter mScreenStateFilter = new IntentFilter();
+
+    private boolean mIsListening = false;
+
+    ScreenReceiver(AODService service) {
+        mService = service;
+        mScreenStateFilter.addAction(Intent.ACTION_SCREEN_ON);
+        mScreenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent.getAction();
+        if (Intent.ACTION_SCREEN_ON.equals(action)) {
+            mService.onDisplayOn();
+        } else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+            mService.onDisplayOff();
+        }
+    }
+
+    void enable() {
+        if (mIsListening) return;
+        if (DEBUG) Log.d(TAG, "Enabling");
+        mService.registerReceiver(this, mScreenStateFilter);
+        mIsListening = true;
+    }
+
+    void disable() {
+        if (!mIsListening) return;
+        if (DEBUG) Log.d(TAG, "Disabling");
+        mService.unregisterReceiver(this);
+        mIsListening = false;
+    }
+
+}

parts/src/org/mokee/device/parts/SettingObserver.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.mokee.device.parts;
+
+import android.database.ContentObserver;
+import android.provider.Settings;
+import android.util.Log;
+
+class SettingObserver extends ContentObserver {
+
+    private static final String TAG = "AODSetting";
+    private static final boolean DEBUG = false;
+
+    private final AODService mService;
+
+    private boolean mIsListening = false;
+
+    SettingObserver(AODService service) {
+        super(null);
+        mService = service;
+    }
+
+    @Override
+    public void onChange(boolean selfChange) {
+        mService.onSettingChange();
+    }
+
+    void enable() {
+        if (mIsListening) return;
+        if (DEBUG) Log.d(TAG, "Enabling");
+        mService.getContentResolver().registerContentObserver(
+            Settings.Secure.getUriFor(Settings.Secure.DOZE_ALWAYS_ON),
+            false, this);
+        mIsListening = true;
+    }
+
+    void disable() {
+        if (!mIsListening) return;
+        if (DEBUG) Log.d(TAG, "Disabling");
+        mService.getContentResolver().unregisterContentObserver(this);
+        mIsListening = false;
+    }
+
+}

parts/src/org/mokee/device/parts/Startup.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.mokee.device.parts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+
+import static mokee.content.Intent.ACTION_INITIALIZE_MK_HARDWARE;
+
+public class Startup extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent.getAction();
+        if (ACTION_INITIALIZE_MK_HARDWARE.equals(action)) {
+            context.startServiceAsUser(
+                    new Intent(context, AODService.class),
+                    UserHandle.CURRENT);
+        }
+    }
+
+}

umtlh/android_device_meizu_sdm845

parts/Android.mk
@@ -1,0 +1,25 @@
+#
+# Copyright (C) 2020 The MoKee Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := MzSDM845Parts
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.lineageos.platform.internal
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)

parts/src/org/lineageos/device/parts/AODService.java
@@ -1,0 +1,92 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.lineageos.device.parts;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+
+public class AODService extends Service {
+
+    private static final String TAG = "AODService";
+    private static final boolean DEBUG = false;
+
+    private static final long AOD_DELAY_MS = 1000;
+
+    private SettingObserver mSettingObserver;
+    private ScreenReceiver mScreenReceiver;
+
+    private Handler mHandler = new Handler(Looper.getMainLooper());
+    private boolean mInteractive = true;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (DEBUG) Log.d(TAG, "Creating service");
+
+        mSettingObserver = new SettingObserver(this);
+        mScreenReceiver = new ScreenReceiver(this);
+
+        mSettingObserver.enable();
+
+        if (Utils.isAODEnabled(this)) {
+            mScreenReceiver.enable();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (DEBUG) Log.d(TAG, "Destroying service");
+
+        mSettingObserver.disable();
+        mScreenReceiver.disable();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    void onSettingChange() {
+        if (Utils.isAODEnabled(this)) {
+            Log.d(TAG, "AOD enabled");
+            mScreenReceiver.enable();
+        } else {
+            Log.d(TAG, "AOD disabled");
+            mScreenReceiver.disable();
+        }
+    }
+
+    void onDisplayOn() {
+        Log.d(TAG, "Device interactive");
+        mInteractive = true;
+        mHandler.removeCallbacksAndMessages(null);
+    }
+
+    void onDisplayOff() {
+        Log.d(TAG, "Device non-interactive");
+        mInteractive = false;
+        mHandler.postDelayed(() -> {
+            if (!mInteractive) {
+                Log.d(TAG, "Trigger AOD");
+                Utils.enterAOD();
+            }
+        }, AOD_DELAY_MS);
+    }
+
+}

parts/src/org/lineageos/device/parts/ScreenReceiver.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.lineageos.device.parts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+
+class ScreenReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "AODScreen";
+    private static final boolean DEBUG = false;
+
+    private final AODService mService;
+
+    private final IntentFilter mScreenStateFilter = new IntentFilter();
+
+    private boolean mIsListening = false;
+
+    ScreenReceiver(AODService service) {
+        mService = service;
+        mScreenStateFilter.addAction(Intent.ACTION_SCREEN_ON);
+        mScreenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent.getAction();
+        if (Intent.ACTION_SCREEN_ON.equals(action)) {
+            mService.onDisplayOn();
+        } else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+            mService.onDisplayOff();
+        }
+    }
+
+    void enable() {
+        if (mIsListening) return;
+        if (DEBUG) Log.d(TAG, "Enabling");
+        mService.registerReceiver(this, mScreenStateFilter);
+        mIsListening = true;
+    }
+
+    void disable() {
+        if (!mIsListening) return;
+        if (DEBUG) Log.d(TAG, "Disabling");
+        mService.unregisterReceiver(this);
+        mIsListening = false;
+    }
+
+}

parts/src/org/lineageos/device/parts/SettingObserver.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.lineageos.device.parts;
+
+import android.database.ContentObserver;
+import android.provider.Settings;
+import android.util.Log;
+
+class SettingObserver extends ContentObserver {
+
+    private static final String TAG = "AODSetting";
+    private static final boolean DEBUG = false;
+
+    private final AODService mService;
+
+    private boolean mIsListening = false;
+
+    SettingObserver(AODService service) {
+        super(null);
+        mService = service;
+    }
+
+    @Override
+    public void onChange(boolean selfChange) {
+        mService.onSettingChange();
+    }
+
+    void enable() {
+        if (mIsListening) return;
+        if (DEBUG) Log.d(TAG, "Enabling");
+        mService.getContentResolver().registerContentObserver(
+            Settings.Secure.getUriFor(Settings.Secure.DOZE_ALWAYS_ON),
+            false, this);
+        mIsListening = true;
+    }
+
+    void disable() {
+        if (!mIsListening) return;
+        if (DEBUG) Log.d(TAG, "Disabling");
+        mService.getContentResolver().unregisterContentObserver(this);
+        mIsListening = false;
+    }
+
+}

parts/src/org/lineageos/device/parts/Startup.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.lineageos.device.parts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+
+import static lineageos.content.Intent.ACTION_INITIALIZE_LINEAGE_HARDWARE;
+
+public class Startup extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent.getAction();
+        if (ACTION_INITIALIZE_LINEAGE_HARDWARE.equals(action)) {
+            context.startServiceAsUser(
+                    new Intent(context, AODService.class),
+                    UserHandle.CURRENT);
+        }
+    }
+
+}