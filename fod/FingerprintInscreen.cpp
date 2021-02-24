MoKee/android_device_meizu_sdm845-common

sdm845: fod: Support screen off auth by listening FOD key event
Change-Id: Iec24d478bb279d5e3999d96b91af362a331edf6f

fod/FingerprintInscreen.cpp
@@ -6,10 +6,11 @@
  *
  */
 
-#define LOG_TAG "FingerprintInscreenService"
+#define LOG_TAG "InscreenService"
 
 #include "FingerprintInscreen.h"
 #include "StellerClientCallback.h"
+#include "KeyEventWatcher.h"
 
 #include <android-base/logging.h>
 #include <cutils/properties.h>
@@ -28,6 +29,9 @@
 #define FOD_POS_X 149 * 3
 #define FOD_POS_Y 531 * 3
 #define FOD_SIZE 62 * 3
+
+#define TOUCHPANAL_DEV_PATH "/dev/input/event4"
+#define KEY_FOD 0x0272
 
 namespace vendor {
 namespace mokee {
@@ -55,11 +59,27 @@ static T get(const std::string& path, const T& def) {
     return file.fail() ? def : result;
 }
 
+static KeyEventWatcher *keyEventWatcher;
+
+static void sighandler(int) {
+    LOG(INFO) << "Exiting";
+    keyEventWatcher->exit();
+}
+
 FingerprintInscreen::FingerprintInscreen()
     : mFingerPressed{false}
+    , mIconShown{false}
     {
     mSteller = ISteller::getService();
     mStellerClientCallback = new StellerClientCallback();
+
+    keyEventWatcher = new KeyEventWatcher(TOUCHPANAL_DEV_PATH, [this](const std::string&, input_event evt) {
+        if (evt.code == KEY_FOD) {
+            notifyKeyEvent(evt.value);
+        }
+    });
+
+    signal(SIGTERM, sighandler);
 }
 
 Return<int32_t> FingerprintInscreen::getPositionX() {
@@ -103,10 +123,12 @@ Return<void> FingerprintInscreen::onRelease() {
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
+    mIconShown = true;
     return Void();
 }
 
 Return<void> FingerprintInscreen::onHideFODView() {
+    mIconShown = false;
     return Void();
 }
 
@@ -134,7 +156,9 @@ Return<bool> FingerprintInscreen::shouldBoostBrightness() {
     return false;
 }
 
-Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallback>&) {
+Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallback>& callback) {
+    std::lock_guard<std::mutex> _lock(mCallbackLock);
+    mCallback = callback;
     return Void();
 }
 
@@ -142,6 +166,31 @@ void FingerprintInscreen::notifyHal(int32_t status, int32_t data) {
     Return<void> ret = this->mSteller->notify(status, data, mStellerClientCallback);
     if (!ret.isOk()) {
         LOG(ERROR) << "notifyHal(" << status << ") error: " << ret.description();
+    }
+}
+
+void FingerprintInscreen::notifyKeyEvent(int value) {
+    if (!mIconShown) {
+        return;
+    }
+
+    LOG(INFO) << "notifyKeyEvent: " << value;
+
+    std::lock_guard<std::mutex> _lock(mCallbackLock);
+    if (mCallback == nullptr) {
+        return;
+    }
+
+    if (value) {
+        Return<void> ret = mCallback->onFingerDown();
+        if (!ret.isOk()) {
+            LOG(ERROR) << "FingerDown() error: " << ret.description();
+        }
+    } else {
+        Return<void> ret = mCallback->onFingerUp();
+        if (!ret.isOk()) {
+            LOG(ERROR) << "FingerUp() error: " << ret.description();
+        }
     }
 }
 

fod/KeyEventWatcher.cpp
@@ -1,0 +1,117 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#define LOG_TAG "KeyEventWatcher"
+
+#include "KeyEventWatcher.h"
+
+#include <android-base/logging.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/epoll.h>
+
+#define EPOLLEVENTS 20
+
+namespace vendor {
+namespace mokee {
+namespace biometrics {
+namespace fingerprint {
+namespace inscreen {
+namespace V1_0 {
+namespace implementation {
+
+static void *work(void *arg);
+
+KeyEventWatcher::KeyEventWatcher(const std::string& file, const WatcherCallback& callback)
+    : mFile(file)
+    , mCallback(callback)
+    , mExit(false)
+    {
+    if (pthread_create(&mPoll, NULL, work, this)) {
+        LOG(ERROR) << "pthread creation failed: " << errno;
+    }
+}
+
+void KeyEventWatcher::exit() {
+    mExit = true;
+    LOG(INFO) << "Exit";
+}
+
+static void *work(void *arg) {
+    int epoll_fd, input_fd;
+    struct epoll_event ev;
+    struct input_event key_event;
+    int nevents = 0;
+
+    LOG(INFO) << "Creating thread";
+
+    KeyEventWatcher *thiz = (KeyEventWatcher *)arg;
+
+    input_fd = open(thiz->mFile.c_str(), O_RDONLY);
+    if (input_fd < 0) {
+        LOG(ERROR) << "Failed opening input dev " << thiz->mFile << ": " << errno;
+        return NULL;
+    }
+
+    ev.events = EPOLLIN;
+    ev.data.fd = input_fd;
+
+    epoll_fd = epoll_create(EPOLLEVENTS);
+    if (epoll_fd == -1) {
+        LOG(ERROR) << "Failed epoll_create: " << errno;
+        goto error;
+    }
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, input_fd, &ev) == -1) {
+        LOG(ERROR) << "Failed epoll_ctl: " << errno;
+        goto error;
+    }
+
+    while (!thiz->mExit) {
+        struct epoll_event events[EPOLLEVENTS];
+
+        nevents = epoll_wait(epoll_fd, events, EPOLLEVENTS, -1);
+        if (nevents == -1) {
+            if (errno == EINTR) {
+                continue;
+            }
+            LOG(ERROR) << "Failed epoll_wait: " << errno;
+            break;
+        }
+
+        for (int i = 0; i < nevents; i++) {
+            int ret;
+            int fd = events[i].data.fd;
+            lseek(fd, 0, SEEK_SET);
+            ret = read(fd, &key_event, sizeof(key_event));
+            if (ret && key_event.type == EV_KEY) {
+                thiz->mCallback(thiz->mFile, key_event);
+            }
+        }
+    }
+
+    LOG(INFO) << "Exiting worker thread";
+
+error:
+    close(input_fd);
+
+    if (epoll_fd >= 0)
+        close(epoll_fd);
+
+    return NULL;
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace inscreen
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace mokee
+}  // namespace vendor

fod/KeyEventWatcher.h
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef VENDOR_MOKEE_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_KEYEVENTWATCHER_H
+#define VENDOR_MOKEE_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_KEYEVENTWATCHER_H
+
+#include <functional>
+#include <string>
+#include <pthread.h>
+#include <linux/input.h>
+
+namespace vendor {
+namespace mokee {
+namespace biometrics {
+namespace fingerprint {
+namespace inscreen {
+namespace V1_0 {
+namespace implementation {
+
+using WatcherCallback = std::function<void(const std::string&, input_event)>;
+
+class KeyEventWatcher {
+  public:
+    KeyEventWatcher(const std::string& file, const WatcherCallback& callback);
+
+    std::string mFile;
+    WatcherCallback mCallback;
+
+    volatile bool mExit;
+
+    void exit();
+
+  private:
+    pthread_t mPoll;
+};
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace inscreen
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace mokee
+}  // namespace vendor
+
+#endif  // VENDOR_MOKEE_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_KEYEVENTWATCHER_H

umtlh/android_device_meizu_sdm845

fod/FingerprintInscreen.cpp
@@ -6,10 +6,11 @@
  *
  */
 
-#define LOG_TAG "FingerprintInscreenService"
+#define LOG_TAG "InscreenService"
 
 #include "FingerprintInscreen.h"
 #include "StellerClientCallback.h"
+#include "KeyEventWatcher.h"
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
@@ -32,6 +33,9 @@
 #define FOD_POS_X 149 * 3
 #define FOD_POS_Y 531 * 3
 #define FOD_SIZE 62 * 3
+
+#define TOUCHPANAL_DEV_PATH "/dev/input/event5"
+#define KEY_FOD 0x0272
 
 namespace vendor {
 namespace lineage {
@@ -61,11 +65,27 @@ static T get(const std::string& path, const T& def) {
     return file.fail() ? def : result;
 }
 
+static KeyEventWatcher *keyEventWatcher;
+
+static void sighandler(int) {
+    LOG(INFO) << "Exiting";
+    keyEventWatcher->exit();
+}
+
 FingerprintInscreen::FingerprintInscreen()
     : mFingerPressed{false}
+    , mIconShown{false}
     {
     mSteller = ISteller::getService();
     mStellerClientCallback = new StellerClientCallback();
+
+    keyEventWatcher = new KeyEventWatcher(TOUCHPANAL_DEV_PATH, [this](const std::string&, input_event evt) {
+        if (evt.code == KEY_FOD) {
+            notifyKeyEvent(evt.value);
+        }
+    });
+
+    signal(SIGTERM, sighandler);
 }
 
 Return<int32_t> FingerprintInscreen::getPositionX() {
@@ -118,12 +138,14 @@ Return<void> FingerprintInscreen::onRelease() {
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
+    mIconShown = true;
     mFODModel = GetProperty("vendor.meizu.fp_vendor", "");
     LOG(INFO) << "mFODModel: " << mFODModel;
     return Void();
 }
 
 Return<void> FingerprintInscreen::onHideFODView() {
+    mIconShown = false;
     return Void();
 }
 
@@ -153,7 +175,9 @@ Return<bool> FingerprintInscreen::shouldBoostBrightness() {
     return false;
 }
 
-Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallback>&) {
+Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallback>& callback) {
+    std::lock_guard<std::mutex> _lock(mCallbackLock);
+    mCallback = callback;
     return Void();
 }
 
@@ -161,6 +185,31 @@ void FingerprintInscreen::notifyHal(int32_t status, int32_t data) {
     Return<void> ret = this->mSteller->notify(status, data, mStellerClientCallback);
     if (!ret.isOk()) {
         LOG(ERROR) << "notifyHal(" << status << ") error: " << ret.description();
+    }
+}
+
+void FingerprintInscreen::notifyKeyEvent(int value) {
+    if (!mIconShown) {
+        return;
+    }
+
+    LOG(INFO) << "notifyKeyEvent: " << value;
+
+    std::lock_guard<std::mutex> _lock(mCallbackLock);
+    if (mCallback == nullptr) {
+        return;
+    }
+
+    if (value) {
+        Return<void> ret = mCallback->onFingerDown();
+        if (!ret.isOk()) {
+            LOG(ERROR) << "FingerDown() error: " << ret.description();
+        }
+    } else {
+        Return<void> ret = mCallback->onFingerUp();
+        if (!ret.isOk()) {
+            LOG(ERROR) << "FingerUp() error: " << ret.description();
+        }
     }
 }
 

fod/KeyEventWatcher.cpp
@@ -1,0 +1,117 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#define LOG_TAG "KeyEventWatcher"
+
+#include "KeyEventWatcher.h"
+
+#include <android-base/logging.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/epoll.h>
+
+#define EPOLLEVENTS 20
+
+namespace vendor {
+namespace lineage {
+namespace biometrics {
+namespace fingerprint {
+namespace inscreen {
+namespace V1_0 {
+namespace implementation {
+
+static void *work(void *arg);
+
+KeyEventWatcher::KeyEventWatcher(const std::string& file, const WatcherCallback& callback)
+    : mFile(file)
+    , mCallback(callback)
+    , mExit(false)
+    {
+    if (pthread_create(&mPoll, NULL, work, this)) {
+        LOG(ERROR) << "pthread creation failed: " << errno;
+    }
+}
+
+void KeyEventWatcher::exit() {
+    mExit = true;
+    LOG(INFO) << "Exit";
+}
+
+static void *work(void *arg) {
+    int epoll_fd, input_fd;
+    struct epoll_event ev;
+    struct input_event key_event;
+    int nevents = 0;
+
+    LOG(INFO) << "Creating thread";
+
+    KeyEventWatcher *thiz = (KeyEventWatcher *)arg;
+
+    input_fd = open(thiz->mFile.c_str(), O_RDONLY);
+    if (input_fd < 0) {
+        LOG(ERROR) << "Failed opening input dev " << thiz->mFile << ": " << errno;
+        return NULL;
+    }
+
+    ev.events = EPOLLIN;
+    ev.data.fd = input_fd;
+
+    epoll_fd = epoll_create(EPOLLEVENTS);
+    if (epoll_fd == -1) {
+        LOG(ERROR) << "Failed epoll_create: " << errno;
+        goto error;
+    }
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, input_fd, &ev) == -1) {
+        LOG(ERROR) << "Failed epoll_ctl: " << errno;
+        goto error;
+    }
+
+    while (!thiz->mExit) {
+        struct epoll_event events[EPOLLEVENTS];
+
+        nevents = epoll_wait(epoll_fd, events, EPOLLEVENTS, -1);
+        if (nevents == -1) {
+            if (errno == EINTR) {
+                continue;
+            }
+            LOG(ERROR) << "Failed epoll_wait: " << errno;
+            break;
+        }
+
+        for (int i = 0; i < nevents; i++) {
+            int ret;
+            int fd = events[i].data.fd;
+            lseek(fd, 0, SEEK_SET);
+            ret = read(fd, &key_event, sizeof(key_event));
+            if (ret && key_event.type == EV_KEY) {
+                thiz->mCallback(thiz->mFile, key_event);
+            }
+        }
+    }
+
+    LOG(INFO) << "Exiting worker thread";
+
+error:
+    close(input_fd);
+
+    if (epoll_fd >= 0)
+        close(epoll_fd);
+
+    return NULL;
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace inscreen
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace lineage
+}  // namespace vendor

fod/KeyEventWatcher.h
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef VENDOR_LINEAGE_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_KEYEVENTWATCHER_H
+#define VENDOR_LINEAGE_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_KEYEVENTWATCHER_H
+
+#include <functional>
+#include <string>
+#include <pthread.h>
+#include <linux/input.h>
+
+namespace vendor {
+namespace lineage {
+namespace biometrics {
+namespace fingerprint {
+namespace inscreen {
+namespace V1_0 {
+namespace implementation {
+
+using WatcherCallback = std::function<void(const std::string&, input_event)>;
+
+class KeyEventWatcher {
+  public:
+    KeyEventWatcher(const std::string& file, const WatcherCallback& callback);
+
+    std::string mFile;
+    WatcherCallback mCallback;
+
+    volatile bool mExit;
+
+    void exit();
+
+  private:
+    pthread_t mPoll;
+};
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace inscreen
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace lineage
+}  // namespace vendor
+
+#endif  // VENDOR_LINEAGE_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_KEYEVENTWATCHER_H