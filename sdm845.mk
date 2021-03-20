922bf75cd88b33b878e8843d7c6214ee6f2345e6
TRANSAERO21 <martynenkodima21@gmail.com>
2021-03-20 12:20:00 +0300

    sdm845: Kill FDE
    Breaks meizu biometrics HAL, WiFi + no recoveries that can decrypt data
    
    Revert "sdm845: Switch to cryptfshw HIDL"
    
    This reverts commit b1c018c7dd087c33bdc84434fb9c3743b666de7f.
    
    Revert "sdm845: Enable TARGET_HW_DISK_ENCRYPTION"
    
    This reverts commit 4d8f4ba8a0c29a4c7a90906985024a069994ad7b.
    
    Revert "sdm845: Set BOARD_USERDATAIMAGE_PARTITION_SIZE to allow FDE encryption"
    
    This reverts commit 1a25b3af726882f2e353cc3bd3f178dc3f830875.
    
    Revert "sdm845: fstab: Enable FDE"
    
    This reverts commit b840c6df69cb1dd51473cbbc276b17d7df9f9c1f.

--- sdm845.mk
+++ sdm845.mk
@@ -225,10 +225,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
