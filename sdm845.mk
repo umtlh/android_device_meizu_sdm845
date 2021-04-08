a646568f298602ebec3229e202b9712cdea8e1c0
YumeMichi <do4suki@gmail.com>
2021-04-08 22:59:13 +0800

    sdm845: wfd: Switch to SDM710 blobs
     * From: PNX_00WW_FIH-user-11-RKQ1.200906.002-00WW_6_100-release-keys
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: Ia2824f2f74edce1a8d3a106cf9f525c9fe13b10f

--- sdm845.mk
+++ sdm845.mk
@@ -337,6 +337,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/wifi,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
+# WiFi Display
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
