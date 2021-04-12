cf1aec5634213b47dceefe4a701a7d825c41e781
TRANSAERO21 <martynenkodima21@gmail.com>
2021-04-13 00:58:29 +0300

    sdm845: m1882: Update proprietary blobs from Flyme 8.21.1.28A Daily (On Android Q)

--- extract-files.sh
+++ extract-files.sh
@@ -84,14 +84,11 @@ function blob_fixup() {
     vendor/lib/libmms_hal_vstab.so | vendor/lib/libmms_warper_vstab.so)
         patchelf --add-needed "libshim_camera.so" "${2}"
         ;;
-    vendor/lib/hw/camera.qcom.so)
+    vendor/lib/camera/components/com.inv.node.eis.so)
         patchelf --add-needed "libprotobuf-cpp-full-vendor-3.9.1.so" "${2}"
         ;;
     etc/vstab_db_0_720p_video_30fps.config | etc/vstab_db_0_4k_video_30fps.config | etc/vstab_db_0_1080p_video_30fps.config | vendor/lib/camera/components/com.arcsoft.node.realtimebokeh.so | vendor/lib/camera/components/com.qti.stats.pdlib.so | vendor/lib/camera/components/com.arcsoft.node.capturebokeh.so | vendor/lib/camera/components/com.arcsoft.node.picauto.so | vendor/lib/camera/components/com.inv.node.eis.so | vendor/lib/camera/components/com.arcsoft.node.hdr.so | vendor/lib/camera/components/com.qti.stats.aec.so | vendor/lib/camera/components/com.arcsoft.node.beauty.so | vendor/lib/camera/components/com.arcsoft.node.smoothtransition.so | vendor/lib/camera/components/com.arcsoft.node.lowlighthdr.so | vendor/lib/libmms_hal_vstab.so | vendor/lib/libarcsoft_dualcam_refocus.so | vendor/lib/hw/com.qti.chi.override.so | vendor/lib/hw/camera.qcom.so)
         sed -i "s|/data/misc/camera|/data/vendor/cumX|g" "${2}"
-        ;;
-    vendor/etc/perf/commonresourceconfigs.xml | vendor/etc/perf/perfboostsconfig.xml | vendor/etc/perf/perfconfigstore.xml | vendor/etc/perf/targetconfig.xml | vendor/etc/perf/targetresourceconfigs.xml)
-        sed -i "s|Target=\"sdm710\"|Target=\"sdm845\"|g" "${2}"
         ;;
     esac
 }
