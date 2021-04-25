c6426e96c5da6870480a3cb0e1e1b6ed92f9ac7b
TRANSAERO21 <martynenkodima21@gmail.com>
2021-04-25 21:40:46 +0000

    sdm845: touch: Nuke
    =(

--- sdm845.mk
+++ sdm845.mk
@@ -286,10 +286,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     telephony-ext
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.meizu_sdm845
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
